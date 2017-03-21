c1e165f3ce607cb1e46dcad85a71dd97b46848b3
Khanh Duong <Khanh Duong>
2017-03-20 23:24:44 -0600

    dynPro2 returns correct answer, but not sure if it is actually DP

--- src/Main.java
+++ src/Main.java
@@ -90,7 +90,6 @@ public class Main {
 					loc[2] = table.get(j).get(i-1).cost + costs[2][i];
 				}
 				
-				
 				table.get(j).get(i).cost = loc[0];
 				table.get(j).get(i).location = 0;
 
@@ -122,17 +121,19 @@ public class Main {
 	}
 	
 	int dynPro2() {
-		ArrayList<ArrayList<Node>> table = new ArrayList<ArrayList<Node>>(12);
+		ArrayList<ArrayList<Node>> table = new ArrayList<ArrayList<Node>>(max_col);
 		Integer[] loc = new Integer[max_row];
 
-		for (int row = 0; row < 12; row++) {
-			table.add(new ArrayList<Node>(3^row * 3));
+		for (int row = 0; row < max_col; row++) {
+			table.add(new ArrayList<Node>((int) (Math.pow(3, row) * 3)));
 		}
-		for (int i = 0; i < max_row; i++) {
-			for (int j = 0; j < max_col; j++) {
+		for (int i = 0; i < table.size(); i++) {
+			for (int j = 0; j < ((int)Math.pow(3, i) * 3); j++) {
 				table.get(i).add(new Node());
 			}
 		}
+		//System.out.println(table.size());
+		//System.out.println(table.get(11).size());
 		
 		table.get(0).get(0).cost = costs[0][0];
 		table.get(0).get(1).cost = costs[1][0];
@@ -142,13 +143,54 @@ public class Main {
 		table.get(0).get(1).location = 1;
 		table.get(0).get(2).location = 2;
 		
-		for (int i = 0; i < 12; i++) {
-			for (int j = 0; j < table.get(i).size(); j++) {
+		//System.out.println(table.size());
+		//System.out.println(table.get(0).size());
+		
+		for (int i = 1; i < max_col; i++) {
+			for (int j = 0; j < table.get(i).size(); j+=3) {
+				if (table.get(i-1).get(j/3).location == 0) {
+					table.get(i).get(j).cost = table.get(i-1).get(j/3).cost + costs[0][i];
+					table.get(i).get(j).location = 0;
+					
+					table.get(i).get(j+1).cost = table.get(i-1).get(j/3).cost + costs[1][i] + 20;
+					table.get(i).get(j+1).location = 1;
+					
+					table.get(i).get(j+2).cost = table.get(i-1).get(j/3).cost + costs[2][i] + 15;
+					table.get(i).get(j+2).location = 2;
+				}
 				
+				if (table.get(i-1).get(j/3).location == 1) {
+					table.get(i).get(j).cost = table.get(i-1).get(j/3).cost + costs[0][i] + 20;
+					table.get(i).get(j).location = 0;
+					
+					table.get(i).get(j+1).cost = table.get(i-1).get(j/3).cost + costs[1][i];
+					table.get(i).get(j+1).location = 1;
+					
+					table.get(i).get(j+2).cost = table.get(i-1).get(j/3).cost + costs[2][i] + 10;
+					table.get(i).get(j+2).location = 2;
+				}
+				
+				if (table.get(i-1).get(j/3).location == 2) {
+					table.get(i).get(j).cost = table.get(i-1).get(j/3).cost + costs[0][i] + 15;
+					table.get(i).get(j).location = 0; 
+					
+					table.get(i).get(j+1).cost = table.get(i-1).get(j/3).cost + costs[1][i] + 10;
+					table.get(i).get(j+1).location = 1;
+					
+					table.get(i).get(j+2).cost = table.get(i-1).get(j/3).cost + costs[2][i];
+					table.get(i).get(j+2).location = 2;
+				}
 			}
 		}
 		
-		return 0;
+		int l = Integer.MAX_VALUE;
+		for (int i = 0; i < table.get(max_col-1).size(); i++) {
+			if (table.get(max_col-1).get(i).cost < l) {
+				l = table.get(max_col-1).get(i).cost;
+			}
+		}
+		
+		return l;
 	}
 
 	public static void main(String[] args) {
@@ -162,7 +204,7 @@ public class Main {
 		System.out.println(l);
 		//System.out.println(m.routes.get(m.totals.indexOf(l)));
 
-		System.out.println(m.dynPro());
+		//System.out.println(m.dynPro());
 		
 		System.out.println(m.dynPro2());
 
